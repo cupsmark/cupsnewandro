fac31c1b2719ef44cab1e0b669c68f1c4c19d9d1
cupsmark <cupstester1@gmail.com>
2017-06-05 11:51:46 +0700

    Fresh Code

---
+++ app/src/main/java/com/cupslicenew/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.cupslicenew;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+}
